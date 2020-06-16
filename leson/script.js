c499e8551b471d8aaad56dd6799eb46cdb9f5631
vadim <vadim.zubairov1993@gmail.com>
2020-06-16 10:10:19 +0500

    'second-leson'

--- leson/script.js
+++ leson/script.js
@@ -8,12 +8,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов:', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов:', '');
-const d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов:', '')
+    const b = prompt('На сколько оцените его?', '')
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if(a != '' && b != '' && a != null && b != null && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done')
+    } else {
+        console.log('error')
+        i--
+    }
+}
 
-console.log(personalMosavieDB);
+if(personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 20) {
+    console.log('Вы классический зритель') 
+} else if (personalMovieDB.count >= 20) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+}
+
+
+console.log(personalMovieDB);
+
+
