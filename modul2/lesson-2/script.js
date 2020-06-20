2e78e6190b963c30c2d9fae85388f184168a1b1f
vadim <vadim.zubairov1993@gmail.com>
2020-06-20 12:24:50 +0500

    'setTimeout'

---
+++ modul2/lesson-2/script.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('.btn');
+let timerId;
+
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+// function logger() {
+//     if(i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('Hello');
+//     i++;
+// }
+
+// let id = setTimeout(function log() {
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// }, 500);
