026d311235b3423b2a932c809a1113f5eb845faf
vadim <vadim.zubairov1993@gmail.com>
2020-06-25 17:17:43 +0500

    'constructor-functions'

---
+++ modul2/lesson-5/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушел`);
+}
+
+const ivan = new User('Ivan', 29);
+const alex = new User('Alex', 22);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
