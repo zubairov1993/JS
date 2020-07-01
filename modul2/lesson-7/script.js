b6398556366639c51aba3a609be3b1144936f1eb
vadim <vadim.zubairov1993@gmail.com>
2020-07-01 19:10:22 +0500

    'modified'

--- modul2/lesson-7/script.js
+++ modul2/lesson-7/script.js
@@ -11,8 +11,25 @@ class Rectangle {
     }
 }
 
-const square = new Rectangle(10, 10);
-const long = new Rectangle(20, 100);
+class ColoredRectangleWidthText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
 
-console.log(square.calcArea());
-console.log(lon.calcArea());
+    showMyProps() {
+        console.log(`Теккст: ${this.text}, color: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWidthText(25, 10, 'Hello, world', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
