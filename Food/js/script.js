5f04fcfc32c26c6150fe459a717d0bcab1f599fa
vadim <vadim.zubairov1993@gmail.com>
2020-07-04 18:33:31 +0500

    'POSt'

--- Food/js/script.js
+++ Food/js/script.js
@@ -215,4 +215,46 @@ document.addEventListener("DOMContentLoaded", () => {
     '.menu .container',
     // 'menu__item'
   ).render();
+
+  //Forms
+
+  const forms = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'Загрузка',
+    success: 'Спасибо! Скоро мы свяжемся с вами',
+    failure: 'Что-то пошло не так...'
+  };
+
+  forms.forEach(item => {
+    postData(item);
+  })
+
+  function postData(form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', '/Food/server.php');
+
+      request.setRequestHeader('Content-type', 'multipart/form-data');
+      const formData = new FormData(form);
+
+      request.send(formData);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
