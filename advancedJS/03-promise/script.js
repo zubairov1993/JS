1843e153da657a5da9f03b6347253d0eee7133fe
vadim <vadim.zubairov1993@gmail.com>
2020-07-08 11:29:24 +0500

    'promise'

---
+++ advancedJS/03-promise/script.js
@@ -0,0 +1,51 @@
+// 'use strict';
+
+// console.log('data request');
+
+// const req = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('data preparation');
+    
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
+    
+//         resolve(product); 
+//     }, 2000);
+// });
+
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//         }, 2000);
+//     });    
+// }).then(data => {
+//     data.modify = true;
+//     return data;
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('error');
+// }).finally(() => {
+//     console.log('finally');
+// });
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time)
+    });
+};
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('ALL');
+// });
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('ALL');
+});
