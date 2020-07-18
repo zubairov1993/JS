8488e3e764b45ff9dd6f661a46eef6a7f8451a89
vadim <vadim.zubairov1993@gmail.com>
2020-07-18 17:20:10 +0500

    'regular-expressions'

---
+++ advancedJS/lesson-6-regular-expressions/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// new RegExp('pattern', 'flags');
+// /pattern/f
+
+// const ans = prompt('Enter your name');
+
+// const reg = /n/;//так мы ищем маленькую букву n
+// const reg = /n/i;//так мы ищем маленькую букву n
+
+//i
+//g - глобальность
+//m
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/./g, "*"));//******* 
+// console.log('12-34-56'.replace(/-/g, ':'));//12:34:56
+
+// const ans = prompt('Enter your name');
+
+// const reg = /n/ig;
+
+// console.log(reg.test(ans));
+
+// \d - это значит что мы ищем цифры
+// \w - ищем все слова
+// \s - ищем все пробелы
+
+// const ans = prompt('Enter your namber');//hhh2hhh
+
+// const reg = /\d/;
+
+// console.log(ans.match(reg));//["2", index: 3, input: "hhh2hhh", groups: undefined]
+
+// const ans = prompt('Enter your namber');//200px
+
+// const reg = /\d/g;
+
+// console.log(ans.match(reg));//["2", "0", "0"]
+
+// const str = 'My name is R2D2';
+
+// console.log(str.match(/\w\d\w\d/i));//["R2D2", index: 11, input: "My name is R2D2", groups: undefined]
+
+// \D - не числа
+// \W - не буквы
+
+// console.log(str.match(/\W/));//[" ", index: 2, input: "My name is R2D2", groups: undefined]
+// console.log(str.match(/\D/ig));//(13) ["M", "y", " ", "n", "a", "m", "e", " ", "i", "s", " ", "R", "D"]
+
+
+
+
+
+
