f74888acb49512b9b413a9d3a484bd22a10084fe
vadim <vadim.zubairov1993@gmail.com>
2020-07-29 16:16:15 +0500

    'function-generator'

---
+++ advancedJS/lesson-13-generator/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// function* generator() {
+//   yield 'S';
+//   yield 'c';
+//   yield 'r';
+//   yield 'i';
+//   yield 'p';
+//   yield 't';
+// }
+
+// const str = generator();
+
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+// console.log(str.next());
+
+function* count(n) {
+  for (let i = 0; i < n; i++) {
+    yield i;
+  }
+}
+
+for (let k of count(7)) {
+  console.log(k);
+}
+
+// const counter = count(7);
+
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
